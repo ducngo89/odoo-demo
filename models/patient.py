9c8183332a1db99d198eb29197ac87bdbcdbbfac
ducngo89 <ducngo89@gmail.com>
2019-11-18 23:10:30 +0700

    search + filter

--- models/patient.py
+++ models/patient.py
@@ -21,9 +21,15 @@ class HospitalPatient(models.Model):
     notes = fields.Text(string='Notes')
     image = fields.Binary(string='Image')
 
+    name = fields.Char(string='Test')
+
     # auto name
-    name_seq = fields.Char(string='Order Reference', required=True,
+    name_seq = fields.Char(string='Patient ID', required=True,
                            copy=False, readonly=True, index=True, default=lambda self: _('New'))
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('fe_male', 'Female')
+    ], default='male', string='Gender')
 
     @api.model
     def create(self, vals):
