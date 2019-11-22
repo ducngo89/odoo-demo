f8e31d6503ccdf6e712e598c96d78276934a8ab6
ducngo89 <ducngo89@gmail.com>
2019-11-22 22:07:54 +0700

    create data file & load default data

--- __manifest__.py
+++ __manifest__.py
@@ -23,6 +23,7 @@
     'data': [
         'security/ir.model.access.csv',
         'data/ir_sequence_data.xml',
+        'data/data.xml',
         'views/patient.xml',
         'views/appointment.xml',
         'reports/report.xml',
