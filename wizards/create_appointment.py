990b765ee4445d54421e6a19884032af55d9e651
ducngo89 <ducngo89@gmail.com>
2019-11-25 13:20:35 +0700

    Create record from code

--- wizards/create_appointment.py
+++ wizards/create_appointment.py
@@ -7,3 +7,11 @@ class CreateAppointment(models.TransientModel):
     _name = "create.appointment"
     patient_id = fields.Many2one('hospital.patient', string="Patient")
     appointment_date = fields.Date(string="Appointment Date")
+
+    def create_appointment(self):
+        vals = {
+            "patient_id": self.patient_id.id,
+            "appointment_date": self.appointment_date,
+            "notes": "Create from wizard/code",
+        }
+        self.env['hospital.appointment'].create(vals)
